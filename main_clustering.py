fdb3086efc4bfe730a817c1537b1f88798225c7a
Zakaria Abdelmoiz Dahi <68249696+Zakaria-Dahi@users.noreply.github.com>
2020-07-30 00:46:36 +0100

    Add files via upload
    - The whole process as a "main" file that calls functions performing either the "1st" or "2nd" steps of clustering.
    - Each function has a parameter to control the display of the information/results of each step separately: 0 for non-verbose, otherwise 1.
    - Step 1:  will return the ideal number of clusters to be used in step 2 (median of elbows).
    - Step 2:
     - Returns the distance between the original clustering using 180 features (15 years x 12 months) and the clustering done considering 15 x 1 features (15 years x mean of the value when considering each of the 5 configurations: 2, 3, 4, 6 and 12 months).
     - Returns also a 1x33 array that contains the ideal number of months to be considered in each series of information. I am not sure if we are going to consider each one of these numbers or just take their median to be applied on all the series in step III.

---
+++ main_clustering.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 09:38:43 2020
+
+"""
+
+from step1 import clustering_step_1 # import the function performing the first step of clustering
+from step2 import clustering_step_2 # import the function performing the second step of clustering
+   
+
+verbose_mode_1 = 0 # step1: 0 do not display anything, 1 for displaying details about the processing
+verbose_mode_2 = 1 # step2: 0 do not display anything, 1 for displaying details about the processing
+
+# execute the 1st step of clustering: extract the ideal number of clusters to be used in step 2
+num_elbows = clustering_step_1(verbose_mode_1) 
+# execute the 2nd step of clustering: extract the ideal number of features to be used in step 3
+num_features = clustering_step_2(num_elbows,verbose_mode_2)
+
